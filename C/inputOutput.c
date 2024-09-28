6bf58ab4f970f42691b9681dcea0a13c62831343
proalamin <pro.alamin247@gmail.com>
2024-09-29 00:54:41 +0600

    simple data input output

---
+++ C/inputOutput.c
@@ -0,0 +1,31 @@
+
+/*
+formate specifire
+
+short int %hd
+int %d
+long int %ld
+long long int %lld
+char %c
+float %f
+double %lf
+long double %Lf
+
+unsigned int %u
+unsigned short %hu
+unsigned long int %lu
+unsigned long long int %llu
+
+*/
+
+#include <stdio.h> 
+
+int main(){
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("a= %d b=%d c= %d", a, b, c);
+
+
+
+    return 0;
+}
