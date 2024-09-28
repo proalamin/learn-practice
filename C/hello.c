353dcc471dccac922798ad5d0ccec636a04e630e
proalamin <pro.alamin247@gmail.com>
2024-09-28 23:42:13 +0600

    day to day learn and practice repo

---
+++ C/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("hello I am hear for bangla size you, be ready!!");
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# learn-practice
+
+### every single day I try to learn something new and try to store it well organized.
