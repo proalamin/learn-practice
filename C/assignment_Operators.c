04b0f721f0a1abc82e167dea25a0d52aca10a43a
proalamin <pro.alamin247@gmail.com>
2024-09-30 01:40:14 +0600

    assignment Operators

--- C/assignment_Operators.c
+++ C/assignment_Operators.c
@@ -12,21 +12,24 @@ Operator	Example	    Same as
 */
 
 
+// Working of assignment operators
 #include <stdio.h>
 int main()
 {
-    int a = 9,b = 4, c;
-    
-    c = a+b;
-    printf("a+b = %d \n",c);
-    c = a-b;
-    printf("a-b = %d \n",c);
-    c = a*b;
-    printf("a*b = %d \n",c);
-    c = a/b;
-    printf("a/b = %d \n",c);
-    c = a%b;
-    printf("Remainder when a divided by b = %d \n",c);
-    
+    int a = 5, c;
+
+    c = a;      // c is 5
+    printf("c = %d\n", c);
+    c += a;     // c is 10 
+    printf("c = %d\n", c);
+    c -= a;     // c is 5
+    printf("c = %d\n", c);
+    c *= a;     // c is 25
+    printf("c = %d\n", c);
+    c /= a;     // c is 5
+    printf("c = %d\n", c);
+    c %= a;     // c = 0
+    printf("c = %d\n", c);
+
     return 0;
 }
