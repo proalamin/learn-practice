31f357ba0dd4c77df90851703fef88342b6b11fc
proalamin <pro.alamin247@gmail.com>
2024-10-01 23:52:43 +0600

    for loop

---
+++ C/10.for_loop.c
@@ -0,0 +1,16 @@
+/*
+for(initialization; condition; update){
+        statement
+    }
+*/
+
+#include <stdio.h>
+
+int main(){
+    
+    for(int i=0; i<3; i++){
+        printf("inside loop\n");
+    }
+
+    return 0;
+}
