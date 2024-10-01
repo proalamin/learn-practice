803e3de862fe2dac8464ed1f55dd2bed8dc45950
proalamin <pro.alamin247@gmail.com>
2024-10-02 00:42:52 +0600

    variables and re-arrange file

--- C/02.variables_dataType.c
+++ C/02.variables_dataType.c
@@ -23,9 +23,9 @@ unsigned long long int %llu
 #include <stdio.h> 
 
 int main(){
-    int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    printf("a= %d b= %d c= %d d= %d", a, b, c, d);
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("a= %d b=%d c= %d", a, b, c);
 
     return 0;
 }
