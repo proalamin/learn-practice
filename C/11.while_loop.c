8527162ac4462197958e1c6ff3d5d37fa0506108
proalamin <pro.alamin247@gmail.com>
2024-10-02 00:14:58 +0600

    while loop

---
+++ C/11.while_loop.c
@@ -0,0 +1,23 @@
+/*
+initialization
+while(condition){
+ statement.....
+ increment/decrement
+
+}
+*/
+
+#include <stdio.h>
+
+int main(){
+    int i=0;
+    while (i<5)
+    {
+        printf("inside while loop\n");
+        i++;
+    }
+    printf("outside while loop\n");
+
+}
+
+
