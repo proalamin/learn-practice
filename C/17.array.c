e02beb645671375fa140ad2306e9d44e69602817
proalamin <pro.alamin247@gmail.com>
2024-10-09 08:40:08 +0600

    array declaration

---
+++ C/17.array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){ 
+
+    //array declaration
+    int a[5]={1,3,4,2,7};
+
+    int b[5]={2,5}; // b[5]={2,5,0,0,0};
+
+    int c[]={3,5}; // here array size is 2
+
+
+
+
+    return 0;
+}
