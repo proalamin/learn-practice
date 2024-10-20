abc3b806c44aedb38232eff045a15b41f1b09e7f
proalamin <pro.alamin247@gmail.com>
2024-10-20 15:09:29 +0600

    Array Reverse comment add

--- C/21.array_reverse.c
+++ C/21.array_reverse.c
@@ -11,10 +11,12 @@ int main(){
         scanf("%d", &a[i]);
     }
 
+    // reverse and a to b
     for(int i=0, j=length-1; i<length; i++, j--){
         b[j]=a[i];
     }
 
+    // again b to a
     for(int i=0; i<length; i++){
         a[i]=b[i];
     }
