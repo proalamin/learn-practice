7b282737bfb1bd82f7d614d396b3be2ad0a9a5af
proalamin <pro.alamin247@gmail.com>
2024-10-20 14:43:57 +0600

    array Value Deleting

---
+++ C/20.array_Value_Deleting.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int a[10000];
+
+int main(){
+    int length;
+    scanf("%d",&length);
+
+    for(int i=0; i<length; i++){
+        scanf("%d", &a[i]);
+    }
+
+    int index;
+    scanf("%d", &index);
+
+    for(int i=index; i<length-1; i++){
+        a[i]=a[i+1];
+    }
+    length--;
+
+    for(int i=0; i<length; i++){
+        printf("%d ", a[i]);
+    }
+    
+
+}
