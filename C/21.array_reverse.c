e3f5f61eeec7473a54bb47c2c88ce0d9b5904c38
proalamin <pro.alamin247@gmail.com>
2024-10-20 15:08:02 +0600

    Array Reverse

---
+++ C/21.array_reverse.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int a[10000];
+int b[10000];
+
+int main(){
+    int length;
+    scanf("%d", &length);
+
+    for(int i=0; i<length; i++){
+        scanf("%d", &a[i]);
+    }
+
+    for(int i=0, j=length-1; i<length; i++, j--){
+        b[j]=a[i];
+    }
+
+    for(int i=0; i<length; i++){
+        a[i]=b[i];
+    }
+
+    for(int i=0; i<length; i++){
+        printf("%d ", a[i]);
+    }
+
+}
