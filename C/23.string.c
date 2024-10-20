8f81d70719368f76617073d3b11bcf7c215dff11
proalamin <pro.alamin247@gmail.com>
2024-10-21 01:44:57 +0600

    Converting String to Integer Number

--- C/23.string.c
+++ C/23.string.c
@@ -10,10 +10,24 @@ int main(){
 
 
     // output
-    fputs(str, stdout);
     // printf("%s", str);
+    fputs(str, stdout);
 
     
+    //Converting String to Integer Number
+    char con[20];
+    int n; // size
+
+    scanf("%d", &n);
+    scanf("%s", con);
+
+    int num=0;
+    for(int i=0; i<n; i++){
+        int digit=con[i]-'0';
+
+        num=num * 10 + digit;
+    }
+    printf("%d", num);
 
     return 0;
 }
