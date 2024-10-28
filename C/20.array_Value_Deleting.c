c689daebdaf2ac386f0611e9470765aa96100a6f
proalamin <pro.alamin247@gmail.com>
2024-10-28 10:08:39 +0600

    array index position delete

--- C/20.array_Value_Deleting.c
+++ C/20.array_Value_Deleting.c
@@ -1,26 +1,34 @@
 #include<stdio.h>
 
-int a[10000];
-
 int main(){
-    int length;
-    scanf("%d",&length);
+    int a[100000];
+    int len;
+    scanf("%d", &len);
 
-    for(int i=0; i<length; i++){
+    for(int i=0; i<len; i++){
         scanf("%d", &a[i]);
     }
 
-    int index;
-    scanf("%d", &index);
+    int delIndex;
+    scanf("%d", &delIndex);
 
-    for(int i=index; i<length-1; i++){
+    if(delIndex<0 || delIndex>=len){
+        printf("index not dave this array");
+    }
+
+//0 1 2 3 -index
+//1 2 3 4
+//1 2 4
+
+    for(int i=delIndex; i<len-1; i++){
         a[i]=a[i+1];
     }
-    length--;
 
-    for(int i=0; i<length; i++){
+    len--;
+    for(int i=0; i<len; i++){
         printf("%d ", a[i]);
     }
-    
 
+
+    return 0;
 }
