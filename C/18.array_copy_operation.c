7e4b6cf245591c9c7533d08732118ff537315341
proalamin <pro.alamin247@gmail.com>
2024-10-28 10:10:45 +0600

    array copy

--- C/18.array_copy_operation.c
+++ C/18.array_copy_operation.c
@@ -1,41 +1,35 @@
 #include<stdio.h>
 
-int a[100000], b[100000];
-
 int main(){
-    int length1=0, length2=0;
+    int a[100000], b[100000];
 
-    scanf("%d", &length1);
+    int len1=0, len2=0;
+    scanf("%d", &len1);
 
-    for(int i=0; i<length1; i++){
+    for(int i=0; i<len1; i++){
         scanf("%d", &a[i]);
     }
 
-    printf("before copy\n");
-    for(int i=0; i<length1; i++){
+    printf("before COPY\n");
+    for(int i=0; i<len1; i++){
         printf("%d ", a[i]);
     }
     printf("\n");
-    for(int i=0; i<length1; i++){
+    for(int i=0; i<len1; i++){
         printf("%d ", b[i]);
     }
 
-    printf("\n");
-    printf("\n");
 
-    for(int i=0; i<length1; i++){
+    printf("\n");
+    printf("AFTER COPY\n");
+    for(int i=0; i<len1; i++){
         b[i]=a[i];
     }
-    length2=length1;
-
-    printf("after copy\n");
-    for(int i=0; i<length1; i++){
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-
-    for(int i=0; i<length1; i++){
+    len2=len1;
+    for(int i=0; i<len2; i++){
         printf("%d ", b[i]);
     }
 
+
+    return 0;
 }
