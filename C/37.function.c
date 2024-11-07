20c3956489d3677ff1501319c088fa3ea9cb66c5
proalamin <pro.alamin247@gmail.com>
2024-11-07 12:42:46 +0600

    void function

--- C/37.function.c
+++ C/37.function.c
@@ -6,12 +6,25 @@ int add(int a, int b){
 }
 
 
+// void function ->when need print result
+void aplhaCheck(char input){
+    if('a'<= input && input<='z'){
+        printf("Lower\n");
+    }else{
+        printf("Upper\n");
+    }
+}
+
+
 int main(){
     int x, y;
     scanf("%d %d", &x, &y);
 
     int result= add(x,y);
-    printf("%d", result);
+    printf("%d\n", result);
+
+    aplhaCheck('a');
+    aplhaCheck('A');
 
     return 0;
 }
