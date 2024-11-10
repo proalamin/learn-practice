a52e1b20b851c819ff5f93014125faff0e688ff7
proalamin <pro.alamin247@gmail.com>
2024-11-10 11:59:08 +0600

    array sum use function

--- C/40.recursion.c
+++ C/40.recursion.c
@@ -20,11 +20,32 @@ void fun2(int n){
 
 }
 
+// array sum
+int a[100000], sum;
+void goTOIndex(int i, int n){
+    if(i == n){
+        return;
+    }
+    sum +=a[i];
+
+    goTOIndex(i+1, n);
+
+}
+
 int main(){
     int n=5;
     int x=1;
     fun(x, n);
     printf("\n");
     fun2(n);
+    printf("\n");
+
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    sum=0;
+    goTOIndex(0, n);
+    printf("%d", sum);
 
 }
