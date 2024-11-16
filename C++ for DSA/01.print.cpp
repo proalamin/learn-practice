b6e7cee314257b984ba99c8e136b3911c089af72
proalamin <pro.alamin247@gmail.com>
2024-11-16 22:55:55 +0600

    rename the file name for sequential serial

---
+++ C++ for DSA/01.print.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main(){
+
+    int id=2234242;
+    char name='C';
+    double cg=4.00;
+
+    std :: cout << id <<std::endl << name <<std::endl<< cg <<std::endl;
+
+    return 0;
+}
