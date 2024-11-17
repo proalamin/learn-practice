5981c737fc9be4d086c0c71fed1fe279387728f0
proalamin <pro.alamin247@gmail.com>
2024-11-17 13:37:36 +0600

    string basic

---
+++ C++ for DSA/09.string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char s[100];
+    cin.getline(s, 100);
+
+    string name;
+    cin >> name;
+
+    cout << s << endl;
+    cout << name << endl;
+
+}
