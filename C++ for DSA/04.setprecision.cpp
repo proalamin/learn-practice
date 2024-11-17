e663e333dc52e77c63f257a0b342c01dba75e79f
proalamin <pro.alamin247@gmail.com>
2024-11-17 12:47:08 +0600

    setprecision

---
+++ C++ for DSA/04.setprecision.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    double d=23.45673;
+    cout << fixed << setprecision(3)<< d << endl; // after (.) how much digit want to print
+
+    return 0;
+}
