5b85f221661fd10de5634670f989d2d063530d20
proalamin <pro.alamin247@gmail.com>
2024-11-17 13:30:35 +0600

    min, max, swap

---
+++ C++ for DSA/08.min_max_swap.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >>b;
+    
+    // if(a<b){
+    //     cout << a <<endl;
+    // }else{
+    //     cout << b << endl;
+    // }
+    cout << min(a,b)<<endl; // min function for find minimum value
+
+    cout << max(a,b) << endl; // max function for maximum value
+
+    swap(a,b);
+    cout << a << " "<< b <<endl;
+
+
+    return 0;
+}
