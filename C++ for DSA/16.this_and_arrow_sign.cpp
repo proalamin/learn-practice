a24876b9105284ab8ee60d872c8e68c745306ca1
proalamin <pro.alamin247@gmail.com>
2024-11-20 17:10:52 +0600

    this and arrow sign

---
+++ C++ for DSA/16.this_and_arrow_sign.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+    public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int roll, int cls, double gpa){
+        (*this).roll =roll;
+        // this->roll=roll;
+        
+        (*this).cls=cls;
+        (*this).gpa=gpa;
+    }
+};
+
+int main(){
+
+    Student rahim(34,5,3.44);
+
+    Student karim(4,5,5);
+
+    cout << rahim.roll << " " << rahim.cls << " " << rahim.gpa << endl;
+    cout << karim.roll << " " << karim.cls << " " << karim.gpa << endl;
+
+
+    return 0;
+}
