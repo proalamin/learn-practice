9964c02678378e5a1a269d6b9e3175aa186353d3
proalamin <pro.alamin247@gmail.com>
2024-12-02 20:08:21 +0600

    min in array of object

---
+++ C++ for DSA/35.min_in_array_of_objects.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+    public:
+    string name;
+    int roll;
+    int mark;
+};
+
+int main(){
+    int n;
+    cin >> n;
+    Student a[n];
+
+    for(int i=0; i<n; i++){
+        cin >> a[i].name >> a[i].roll>>a[i].mark;
+    }
+
+    // int mn = INT_MAX;
+    Student mn;
+    mn.mark=INT_MAX;
+    for(int i=0; i<n; i++){
+        if(a[i].mark < mn.mark){
+            mn= a[i];
+        }
+    }
+    cout << mn.name << " " << mn.roll << " "<< mn.mark << endl;
+    
+
+
+    return 0;
+}
