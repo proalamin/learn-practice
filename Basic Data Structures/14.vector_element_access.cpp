ccb64185c791c94e3902f1399748c3081150c23f
proalamin <pro.alamin247@gmail.com>
2024-12-11 00:04:13 +0600

    vector element access

---
+++ Basic Data Structures/14.vector_element_access.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    vector<int> v={1,2,3,4,5};
+    cout << v[3] << endl;
+
+    cout << v.back() << endl; //Access the last element.
+
+    
+    cout << v[0] << endl; // Access the first element.
+    cout << v.front() << endl; // Access the first element.
+
+
+
+    return 0;
+}
