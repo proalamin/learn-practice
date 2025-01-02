956a6076f05e16f3a2f631510270c08a208efc42
proalamin <pro.alamin247@gmail.com>
2025-01-02 18:25:34 +0600

    List operation functions

--- Basic Data Structures/49.list_operation.cpp
+++ Basic Data Structures/49.list_operation.cpp
@@ -2,9 +2,24 @@
 using namespace std;
 
 int main(){
-
     
-
-
+    list <int> l= {1,2,3,4,5,6,5};
+    // l.remove(1);
+    // l.sort();
+    // l.sort(greater<int>());
+    // l.unique(); // Deletes the duplicate values from the list. You must sort the list first.
+    l.reverse();
+    
+    
+    for(int val : l){
+        cout << val << " ";
+    }
+    
     return 0;
 }
+
+/*
+Operations functions-
+https://docs.google.com/document/d/1IbS-qmFx6oRO-GyIG55yavdDUHpfHTNM/edit
+ 
+*/
