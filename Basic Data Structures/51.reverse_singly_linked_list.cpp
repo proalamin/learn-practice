9b1cef41387b6b71cca65548b0a9263c6e98aa6a
proalamin <pro.alamin247@gmail.com>
2025-01-03 18:05:20 +0600

    reverse singly linked list

---
+++ Basic Data Structures/51.reverse_singly_linked_list.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val){
+    Node* newNode= new Node(val);
+
+    //Corner case-> if linked list is empty
+    if(head== NULL){
+        head =newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next= newNode;
+    tail = newNode;
+    
+}
+
+void printLinkedList(Node* head){
+    Node* temp=head;
+    while(temp != NULL){
+        cout << temp->val << endl;
+        temp= temp->next;
+    }
+}
+
+void reverse_linked_list(Node* &head, Node* &tail, Node* temp){
+    // base case
+    if(temp->next == NULL){
+        head=temp;
+        return;
+    }
+    reverse_linked_list(head,tail, temp->next);
+    temp->next->next=temp;
+    temp->next= NULL;
+    tail=temp;
+}
+
+
+int main(){
+    
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    int val;
+    while(1){
+        cin >> val;
+        if(val == -1){
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+
+
+    reverse_linked_list(head,tail, head);
+    printLinkedList(head);
+    
+    
+    return 0;
+}
