2da5b48cf2025357963f7aef3e46b82d35f04661
proalamin <pro.alamin247@gmail.com>
2025-01-05 13:59:07 +0600

    stl list

--- Basic Data Structures/46.stl_list.cpp
+++ Basic Data Structures/46.stl_list.cpp
@@ -1,3 +1,8 @@
+/*
+List Built-in Functions: 
+https://docs.google.com/document/d/1IbS-qmFx6oRO-GyIG55yavdDUHpfHTNM/edit
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -12,18 +17,3 @@ int main(){
     return 0;
 }
 
-/*
-List Built-in Functions: 
-
-
-Constructor
-
-Name                                    Details                                                                     Time Complexity
-list<type>myList;                       Construct a list with 0 elements.                                           O(1)
-list<type>myList(N);                    Construct a list with N elements and the value will be garbage.             O(N)
-list<type>myList(N,V);                  Construct a list with N elements and the value will be V.                   O(N)
-list<type>myList(list2);                Construct a list by copying another list list2.                             O(N)
-list<type>myList(A,A+N);                Construct a list by copying all elements from an array A of size N.         O(N)
-list<type>myList(v.begin(),v.end());    Construct a list by copying all elements from a vector v.                   O(N)
-
-*/
