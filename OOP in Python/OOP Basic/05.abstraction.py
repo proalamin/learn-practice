8f760e8d64cdce47cc927960a59028c5b02b1b28
proalamin <pro.alamin247@gmail.com>
2025-04-15 01:15:59 +0600

    abstraction

---
+++ OOP in Python/OOP Basic/05.abstraction.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+    def perimeter(self):
+        return self.length * self.width
+
+class Circle(Shape):
+    pass
+
+rect = Rectangle(5, 6)
+print(rect.area())
+
+
