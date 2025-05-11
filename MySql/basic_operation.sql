281aa798c7b92c8201425f16b6d12e66dc3f8d89
proalamin <pro.alamin247@gmail.com>
2025-05-11 11:35:01 +0600

    create operation

--- MySql/basic_operation.sql
+++ MySql/basic_operation.sql
@@ -1,6 +1,18 @@
-create database people1;
+create database people;
 
 show databases;
-use people1;
+use people;
 
-drop database people1;
+-- drop database people; -- delete database
+
+-- create operation
+
+create table people(
+	id int primary key,
+    name varchar(50),
+    email varchar(100),
+    salary float
+);
+
+drop table people;
+
