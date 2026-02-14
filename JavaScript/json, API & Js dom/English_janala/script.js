9093f3488942c4113147c3873a969f8e78129c30
proalamin <pro.alamin247@gmail.com>
2026-02-14 20:34:46 +0600

    Js -> Handle Errors , empty lessons for Better UX

--- JavaScript/json, API & Js dom/English_janala/script.js
+++ JavaScript/json, API & Js dom/English_janala/script.js
@@ -1,22 +1,73 @@
-const loadLessons = () =>{
-    fetch("https://openapi.programming-hero.com/api/levels/all")
-        .then((res)=> res.json())
-        .then((data) => displayLessons(data.data))
-}
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((data) => displayLessons(data.data));
+};
 
 const displayLessons = (lessons) => {
-    const levelContainer = document.getElementById("level-container");
-    levelContainer.innerHTML="";
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
 
-    for(let lesson of lessons){
-        const btnDiv= document.createElement("div");
-        btnDiv.innerHTML=`
-            <button class="rounded border border-[#422ad5] px-4 py-2 text-sm font-semibold text-[#422ad5]">Lesson-${lesson.level_no}</button>
-        `
-        levelContainer.appendChild(btnDiv);
-    }
-}
+  for (let lesson of lessons) {
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+            <button onclick="loadLevelWord(${lesson.level_no})" class="rounded border border-[#422ad5] px-4 py-2 text-sm font-semibold text-[#422ad5]">Lesson-${lesson.level_no}</button>
+        `;
+    levelContainer.append(btnDiv);
+  }
+};
 
+const loadLevelWord = (id) => {
+  console.log(`id - ${id}`);
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWorld(data.data));
+};
+const displayLevelWorld = (words) => {
+  const wordsContainer = document.getElementById("word-container");
+  wordsContainer.innerHTML = "";
 
+  if(words.length == 0){
+    wordsContainer.innerHTML = `
+      <div class="text-center col-span-full py-10 space-y-2">
+                        <img class="mx-auto" src="./assets/alert-error.png" alt="">
+                        <p class="font-bangla text-lg font-medium text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                        <h2 class="font-bangla text-4xl font-semibold">নেক্সট Lesson এ যান</h2>
+                    </div>
+    `;
+    // alert("t");
+    return;
+  }
+
+  words.forEach((word) => {
+    console.log(word);
+    const cardDiv = document.createElement("div");
+    cardDiv.innerHTML = `
+        <div class="space-y-4 rounded-xl bg-white px-4 py-10 text-center shadow-sm sm:px-5 md:py-12">
+                        <h2 class="text-xl font-bold md:text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায় নি"}</h2>
+                        <p class="text-sm font-semibold text-zinc-500 md:text-base">Meaning / Pronounciation</p>
+                        <div class="font-bangla text-xl font-medium text-zinc-600 md:text-2xl">"${word.meaning ? word.meaning : "আর্থ পাওয়া যায় নি" } / ${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায় নি"}"</div>
+                        <div class="flex justify-between items-center">
+                            <button class="btn-primary rounded-md bg-[#1A91FF40] px-3 py-2 hover:bg-[#1A91FF70] md:px-4">
+                                <i class="fa-solid fa-circle-info"></i>
+                            </button>
+                            <button class="btn-primary rounded-md bg-[#1A91FF40] px-3 py-2 hover:bg-[#1A91FF70] md:px-4">
+                                <i class="fa-solid fa-volume-high"></i>
+                            </button>
+                        </div>
+                </div>
+        `;
+    wordsContainer.append(cardDiv);
+  });
+};
+
+// {
+//     "id": 5,
+//     "level": 1,
+//     "word": "Eager",
+//     "meaning": "আগ্রহী",
+//     "pronunciation": "ইগার"
+// }
 
 loadLessons();
