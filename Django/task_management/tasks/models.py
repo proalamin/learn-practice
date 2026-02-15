8fe1cc3de305f82d5299ff144b0df3fd96c0ec06
proalamin <pro.alamin247@gmail.com>
2026-02-15 16:24:53 +0600

    task management-> Project class add in model with one to many relationship

--- Django/task_management/tasks/models.py
+++ Django/task_management/tasks/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
+
+
 class Task(models.Model):
+    project = models.ForeignKey("Project", on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=255)
     description =models.TextField()
     due_date= models.DateField()
