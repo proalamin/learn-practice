570ab002cecce5db4545920b428e668f422a0975
proalamin <pro.alamin247@gmail.com>
2026-02-15 14:36:30 +0600

    task management-> create class in model

--- Django/task_management/tasks/models.py
+++ Django/task_management/tasks/models.py
@@ -3,4 +3,28 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=255)
+    description =models.TextField()
+    due_date= models.DateField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    
+# one to one
+# many to one
+# many to many
+
+class TaskDetail(models.Model):
+    HIGH ='H'
+    MEDIUM = 'M'
+    LOW = 'L'
+    PRIORITY_OPTIONS = (
+        (HIGH,"High"),
+        (MEDIUM,"Medium"),
+        (LOW, "Low")
+    )
+    task = models.OneToOneField(Task, on_delete=models.CASCADE)
+    assigned_to=models.CharField(max_length=100)
+    priority= models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
