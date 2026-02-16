06b389fd7c9dd87f77e218bd7a3f35ad818029aa
proalamin <pro.alamin247@gmail.com>
2026-02-16 15:50:58 +0600

    task management-> create employee class

--- Django/task_management/tasks/models.py
+++ Django/task_management/tasks/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+
 class Project(models.Model):
     name = models.CharField(max_length=100)
     start_date = models.DateField()
@@ -9,6 +14,9 @@ class Project(models.Model):
 
 class Task(models.Model):
     project = models.ForeignKey("Project", on_delete=models.CASCADE, default=1)
+    
+    assigned_to = models.ManyToManyField(Employee)
+    
     title = models.CharField(max_length=255)
     description =models.TextField()
     due_date= models.DateField()
@@ -35,4 +43,4 @@ class TaskDetail(models.Model):
     assigned_to=models.CharField(max_length=110)
     priority= models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
     
-    
+
