e1703d09cb789bceef097830114c0b33ad150fac
proalamin <pro.alamin247@gmail.com>
2026-02-18 12:52:44 +0600

    task management-> learn and implement  django form

--- Django/task_management/tasks/views.py
+++ Django/task_management/tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tasks.forms import TaskForm
+from tasks.models import Employee, Task
 
 def manager_dashboard(request):
     return render(request, 'dashboard/manager_dashboard.html')
@@ -18,4 +20,28 @@ def test(request):
 
 
 def create_task(req):
-    return render(req, "test_form.html")
+    employees = Employee.objects.all()
+    form =TaskForm(employees=employees) # For GET
+    
+    if req.method == "POST":
+        form =TaskForm(req.POST, employees=employees)
+        # print(form)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            data= form.cleaned_data
+            title = data.get('title')
+            description= data.get('description')
+            due_date= data.get("due_date")
+            assigned_to = data.get('assigned_to')
+            
+            task = Task.objects.create(title= title, description=description, due_date=due_date)
+            
+            # assign employee to task
+            for emp_id in assigned_to:
+                employee = Employee.objects.get(id=emp_id)
+                task.assigned_to.add(employee)
+                
+            return HttpResponse("Task added successfully")
+
+    context = {"form": form}
+    return render(req, "test_form.html", context)
