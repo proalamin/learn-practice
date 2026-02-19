b54090456d244b140a8cda6f1ffa00c9438a096f
proalamin <pro.alamin247@gmail.com>
2026-02-19 12:26:21 +0600

    task management-> Data Queries

--- Django/task_management/tasks/views.py
+++ Django/task_management/tasks/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm, TaskModelForm
-from tasks.models import Employee, Task
+from tasks.models import Employee, Task, TaskDetail
+from datetime import date
+from django.db.models import Q
 
 def manager_dashboard(request):
     return render(request, 'dashboard/manager_dashboard.html')
@@ -41,16 +43,33 @@ def create_task(request):
 
 def view_task(req):
     # retrieve all data from task model
-    tasks = Task.objects.all()
+    # tasks = Task.objects.all()
     
     # retrieve a specific task
-    task_3 = Task.objects.get(pk=2)
+    # task_3 = Task.objects.get(pk=2)
     
     # retrieve a specific task
-    first_task = Task.objects.first()
+    # first_task = Task.objects.first()
     
     
-    return render(req, 'show_task.html', 
-                  {"tasks": tasks, "task_3": task_3, "first_task": first_task}
-    )
+    # return render(req, 'show_task.html', {"tasks": tasks, "task_3": task_3, "first_task": first_task})
     # return HttpResponse("ok")
+    
+    pending_tasks= Task.objects.filter(status = "PENDING")
+    
+    # show the task which due date today
+    due_date_today= Task.objects.filter(due_date= date.today())
+    
+    # show the task which priority is not low
+    priority_not_low= TaskDetail.objects.exclude(priority = 'L')
+    
+    # show the task that contain word c and status PENDING""
+    words= Task.objects.filter(title__icontains = 'c', status= 'PENDING')
+    
+    # show the task that contain word c OR status PENDING""
+    words_or= Task.objects.filter(Q(title__icontains = 'c') | Q(status= 'PENDING'))
+        
+    return render(req, 'show_task.html', {'pending_tasks': pending_tasks, 'due_date_today':due_date_today, "priority_not_low": priority_not_low, 'words': words, "words_or": words_or})
+    
+    
+    
