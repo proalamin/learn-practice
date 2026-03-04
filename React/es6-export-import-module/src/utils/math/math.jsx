c665407e215c8939aaa68ef1658d5738281e3f92
proalamin <pro.alamin247@gmail.com>
2026-03-04 10:10:10 +0600

    es6 export import

---
+++ React/es6-export-import-module/src/utils/math/math.jsx
@@ -0,0 +1,6 @@
+const add =(a, b)=> a+b;
+const diff =(a, b)=> a-b;
+
+const multiply =(a, b)=>a*b;
+
+export { add, diff, multiply as mul}
---
+++ React/es6-export-import-module/src/utils/tax/tax.jsx
@@ -0,0 +1,5 @@
+import { mul } from "../math/math";
+
+const calcuTax= mul(2000, .15);
+
+export {calcuTax}
