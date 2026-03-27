3a6715f2817283c864cebc142ccd06db8b3adccd
proalamin <pro.alamin247@gmail.com>
2026-03-27 08:15:03 +0600

    models create and requirements file added

--- Django/amin_shop_drf/order/models.py
+++ Django/amin_shop_drf/order/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from users.models import User
+from product.models import Product
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Cart of {self.user.username}"
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField()
+    
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+    
+    
+class Order(models.Model):
+    PENDING = 'Pending'
+    SHIPPED = 'Shipped'
+    DELIVERED = 'Delivered'
+    STATUS_CHOICE =[
+        (PENDING, 'Pending'),
+        (SHIPPED, 'Shipped'),
+        (DELIVERED, 'Delivered'),
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICE, default=PENDING)
+    total_price= models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"order {self.id} by {self.user.username}"
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
--- Django/amin_shop_drf/users/models.py
+++ Django/amin_shop_drf/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from users.managers import CustomUserManager
+ 
 class User(AbstractUser):
     username =None
     email = models.EmailField(unique=True)
