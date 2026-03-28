08b95bcf3ca48d48fc3229ea8520c34abae97b21
proalamin <pro.alamin247@gmail.com>
2026-03-28 14:43:39 +0600

    ModelSerializer implemented

--- Django/amin_shop_drf/product/serializers.py
+++ Django/amin_shop_drf/product/serializers.py
@@ -1,28 +1,53 @@
 from rest_framework import serializers
 from decimal import Decimal
-from product.models import Category
+from product.models import Category, Product
 
 
-class CategorySerializer(serializers.Serializer):
-    id= serializers.IntegerField()
-    name= serializers.CharField()
-    description= serializers.CharField()
+# class CategorySerializer(serializers.Serializer):
+#     id= serializers.IntegerField()
+#     name= serializers.CharField()
+#     description= serializers.CharField()
 
-class ProductSerializers(serializers.Serializer):
-    id = serializers.IntegerField()
-    name= serializers.CharField()
-    unit_price = serializers.DecimalField(max_digits=10, decimal_places=2, source='price')
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Category
+        fields= ['id', 'name', 'description']
+
+
+# class ProductSerializers(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     name= serializers.CharField()
+#     unit_price = serializers.DecimalField(max_digits=10, decimal_places=2, source='price')
     
-    price_with_tax= serializers.SerializerMethodField(method_name='cal_tax')
+#     price_with_tax= serializers.SerializerMethodField(method_name='cal_tax')
     
-    # category= serializers.PrimaryKeyRelatedField(
-    #     queryset= Category.objects.all()
-    # )
-    # category = CategorySerializer()
+#     # category= serializers.PrimaryKeyRelatedField(
+#     #     queryset= Category.objects.all()
+#     # )
+#     # category = CategorySerializer()
+#     category = serializers.HyperlinkedRelatedField(
+#         queryset= Category.objects.all(),
+#         view_name = 'specific-category',
+#     )
+    
+    # def cal_tax(self, product):
+    #     return round(product.price * Decimal(1.1), 2)
+
+class ProductSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        # fields= '__all__'
+        fields= ['id', 'name', 'description', 'price','price_with_tax', 'stock', 'category']
+    
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='cal_tax'
+    )
+    
     category = serializers.HyperlinkedRelatedField(
         queryset= Category.objects.all(),
         view_name = 'specific-category',
     )
+    
     
     def cal_tax(self, product):
         return round(product.price * Decimal(1.1), 2)
