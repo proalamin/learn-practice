500e28fcc288966b55962f8208cca1134ffe1229
proalamin <pro.alamin247@gmail.com>
2026-04-12 12:44:35 +0600

    feat: add total price calculation to CartItem and Cart serializers

--- Django/amin_shop_drf/order/serializers.py
+++ Django/amin_shop_drf/order/serializers.py
@@ -13,22 +13,31 @@ class ProductSerializerForCart(serializers.ModelSerializer):
 class CartItemSerializer(serializers.ModelSerializer):
     # product = ProductSerializers()
     product = ProductSerializerForCart()
+    total_Price=serializers.SerializerMethodField(method_name='get_total_price')
     
     # product_price=serializers.SerializerMethodField(method_name='get_product_price')
     
     class Meta:
         model= CartItem
-        fields = ['id', 'product', 'quantity', 'product']
+        fields = ['id', 'product', 'quantity', 'product', 'total_Price']
     
     # def get_product_price(self, cart_item):
     #     return cart_item.product.price
+    
+    def get_total_price(self, cart_item: CartItem):
+        return cart_item.quantity * cart_item.product.price
         
         
 class CartSerializers(serializers.ModelSerializer):
     items = CartItemSerializer(many=True)
+    total_price= serializers.SerializerMethodField(method_name="get_total_price")
     
     class Meta:
         model =Cart
-        fields = ['id', 'user', 'items']
+        fields = ['id', 'user', 'items', 'total_price']
         
+    def get_total_price(self, cart: Cart):
+        list = sum([item.product.price * item.quantity for item in cart.items.all()])
+        # print("list", list)
+        return list;
     
